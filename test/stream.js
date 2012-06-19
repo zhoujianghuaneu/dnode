d394db7033e86cf9c65ea3fdc54923df07f30347
James Halliday <mail@substack.net>
2012-06-19 07:07:13 -0700

    test for streams that passes

--- test/stream.js
+++ test/stream.js
@@ -3,19 +3,22 @@ var net = require('net');
 var test = require('tap').test;
 
 test('stream', function (t) {
-    t.plan(1);
+    t.plan(2);
     var port = Math.floor(Math.random() * 40000 + 10000);
     
     var server = net.createServer(function (stream) {
         var d = dnode({
             meow : function f (g) { g('cats') }
         });
+        d.on('remote', function (remote) {
+            t.equal(remote.x, 5);
+        });
         stream.pipe(d).pipe(stream);
     });
     server.listen(port);
     
     server.on('listening', function () {
-        var d = dnode();
+        var d = dnode({ x : 5 });
         d.on('remote', function (remote) {
             remote.meow(function (cats) {
                 t.equal(cats, 'cats');

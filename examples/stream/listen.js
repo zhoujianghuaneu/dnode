1e706442ff8bd112a5ad9c9c935ea381d7878b38
James Halliday <mail@substack.net>
2012-06-19 23:16:15 -0700

    drop the new in the stream example

--- examples/stream/listen.js
+++ examples/stream/listen.js
@@ -2,7 +2,7 @@ var dnode = require('../../');
 var net = require('net');
 
 var server = net.createServer(function (c) {
-    var d = new dnode({
+    var d = dnode({
         transform : function (s, cb) {
             cb(s.replace(/[aeiou]{2,}/, 'oo').toUpperCase())
         }

174a7c0f6701749a5b4d4b8ef298f3190b582a26
James Halliday <mail@substack.net>
2012-12-17 03:10:22 -0800

    fix a bug where multiple messages could arrive in the same chunk

--- test/simple.js
+++ test/simple.js
@@ -1,9 +1,8 @@
 var dnode = require('../');
-var test = require('tap').test;
+var test = require('tape');
 
-test('simple', function (t) {
-    t.plan(7);
-    var port = Math.floor(Math.random() * 40000 + 10000);
+test('simple server and client', function (t) {
+    t.plan(5);
     
     var server = dnode({
         timesTen : function (n,reply) {
@@ -15,25 +14,26 @@ test('simple', function (t) {
             t.equal(n, 5);
             cb(n * 10);
         },
-    }).listen(port.toString()); // test for stringified ports too why not
+    });
     
-    server.on('listening', function () {
-        dnode.connect(port, function (remote, conn) {
-            t.ok(conn.id);
-            t.equal(conn.stream.remoteAddress, '127.0.0.1');
-            
-            remote.moo(function (x) {
-                t.equal(x, 100, 'remote moo == 100');
-            });
-            remote.sTimesTen(5, function (m) {
-                t.equal(m, 50, '5 * 10 == 50');
-                remote.timesTen(m, function (n) {
-                    t.equal(n, 500, '50 * 10 == 500');
-                    conn.end();
-                    server.close();
-                    t.end();
-                });
+    var client = dnode();
+    client.on('remote', function (remote) {
+        remote.moo(function (x) {
+            t.equal(x, 100, 'remote moo == 100');
+        });
+        remote.sTimesTen(5, function (m) {
+            t.equal(m, 50, '5 * 10 == 50');
+            remote.timesTen(m, function (n) {
+                t.equal(n, 500, '50 * 10 == 500');
             });
         });
     });
+    
+    server.on('end', function () {
+        console.log('server END');
+    });
+    client.on('end', function () {
+        console.log('client END');
+    });
+    server.pipe(client).pipe(server);
 });
